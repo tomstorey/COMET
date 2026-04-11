0eee578067e6f11fbd8609b116a804e542b81afc
Tom Storey <7754310+tomstorey@users.noreply.github.com>
2026-04-11 08:37:29 +0100

    A little tidying up
    * Remove the /'s at the end of some signal names because the inputs are no longer explicitly active low logic
    * Make the BG input being asserted a condition that causes the timer to be reset

--- rtl/COMET68k/v3/bus_watchdog.sv
+++ rtl/COMET68k/v3/bus_watchdog.sv
@@ -1,20 +1,23 @@
 /* Bus Watchdog
  *
- * The bus watchdog monitors the AS/ signal, and when ever it is asserted, a timer is started. If
- * that timer reaches its terminal count while AS/ is still asserted, the watchdog will assert
- * BERR/ to end the current bus cycle.
+ * The bus watchdog monitors the AS signal, and when ever it is asserted, a timer is started. If
+ * that timer reaches its terminal count while AS is still asserted, the watchdog will assert
+ * BERR to end the current bus cycle.
+ *
+ * BG can be used to disable the watchdog during bus grants to peripherals which do not support
+ * bus errors for cycle termination.
  *
  * The following bus cycle types are covered by this watchdog:
  *
  * Cycle type               Normal termination method               Timeout side effect
  * ----------------------   -------------------------------------   --------------------------------
- * Memory or peripheral     DTACK/ asserted                         Bus error exception
- * Vectored interrupt       DTACK/ asserted by interruptor          Spurious interrupt exception
- * Autovectored interrupt   VPA/ asserted by interrupt controller   Spurious interrupt exception
+ * Memory or peripheral     DTACK asserted                          Bus error exception
+ * Vectored interrupt       DTACK asserted by interruptor           Spurious interrupt exception
+ * Autovectored interrupt   VPA asserted by interrupt controller    Spurious interrupt exception
  *                          (internally asserted, or externally
  *                           through n_autovec signal)
  *
- * A reset input is not used, because negation of AS/ causes the timer to reset, and AS/ will negate
+ * A reset input is not used, because negation of AS causes the timer to reset, and AS will negate
  * during system reset.
  */
 `default_nettype none
@@ -31,7 +34,7 @@ module bus_watchdog
 	logic [BITS-1:0] counter;
 	
 	always_ff @(posedge cpu_clk) begin
-		if (!as) begin
+		if (!as || bg) begin
 			counter <= '0;
 		end
 		else begin
@@ -46,6 +49,6 @@ module bus_watchdog
 	
 	always_comb begin
 		/* Assert BERR once the terminal count is reached */
-		berr = (&counter) & !bg;
+		berr = (&counter);
 	end
 endmodule /* bus_watchdog */
